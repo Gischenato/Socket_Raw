d34ec86ca9302a20a7bf1192fd0749450d9052b6
Lucas Dellatorre <lucasdellatorre222@gmail.com>
2023-10-09 23:13:37 -0300

    finish

---
+++ README.md
@@ -0,0 +1,54 @@
+# User guide
+
+## Client side
+
+```
+    python <server_ip> <tcp | udp>
+```
+
+## Server side
+
+```
+    python <server-ip>
+```
+
+## Client Commands
+### Login
+
+    Set a username for a client
+
+```
+    /login <user>
+```
+
+### Private Message
+
+    Direct message for a user.
+
+```
+    /pm <username> <message>
+```
+
+### Private Message With File
+
+    Transfer a file for a user.
+
+```
+    /pmf <username> <filepath>
+```
+
+### Broadcast message 
+
+    Emit a message for all users in the application.
+
+```
+    /all <message>
+```
+
+### Logout
+
+    Leave the application.
+
+```
+    /logout
+```
--- client.py
+++ client.py
@@ -1,83 +1,166 @@
-from socket import socket, AF_INET, SOCK_DGRAM
+from socket import socket, timeout, AF_INET, SOCK_DGRAM, SOCK_STREAM
 import threading
 from sys import argv
 import base64
 
-server_ip = argv[1] 
-server_port = argv[2] 
-USER_SOCKET = socket(AF_INET, SOCK_DGRAM)
-SERVER_ADDR = (server_ip, int(server_port))
-USER_SOCKET.settimeout(1)
-
 generate_message = lambda data: '[' + ", ".join(list(map(replace_coma, data))) + ']'
+
 
 replace_coma = lambda txt: str(txt).replace(',', '%;')
 
-def generate_file_message(file_path):
-    with open(file_path, 'rb') as file:
-        content = file.read()
-    return base64.b64encode(content)
+class Client:
+    def __init__(self, server_ip, server_port, socket_type='tcp'):
+        self.SOCKET_TYPE = socket_type
+        self.SERVER_IP = server_ip
+        self.SERVER_PORT = int(server_port)
+        self.DATA_SOCKET = socket(AF_INET, SOCK_STREAM) if socket_type == 'tcp' else socket(AF_INET, SOCK_DGRAM)
+        self.CONTROLL_SOCKET = socket(AF_INET, SOCK_STREAM) if socket_type == 'tcp' else socket(AF_INET, SOCK_DGRAM)
+        self.file_counter = 0
+        self.username = None
 
-def send_message(msg):
-    if msg[0] == "pm":
+
+    def connect(self):
+        print(f'connecting to {self.SERVER_IP}:{self.SERVER_PORT}')
+        self.CONTROLL_SOCKET.connect((self.SERVER_IP, self.SERVER_PORT + 2))
+        self.DATA_SOCKET.connect((self.SERVER_IP, self.SERVER_PORT + 3))
+    
+    def is_base64(self, message):
+        message = message.split("b'")[1].replace("'", "")
+        decoded_message = base64.b64decode(message)
+        return decoded_message
+    
+    def close(self):
+        self.CONTROLL_SOCKET.close()
+        self.DATA_SOCKET.close()
+
+    def send_message(self, msg, socket:socket):
         msg = generate_message(msg)
-        USER_SOCKET.sendto(msg.encode(), SERVER_ADDR) # encode utf-8
-    elif msg[0] == "pmf": 
+        if self.SOCKET_TYPE == 'tcp':
+            socket.send(msg.encode())
+        else:
+            socket.sendto(msg.encode(), (self.SERVER_IP, self.SERVER_PORT))
 
-        msg = generate_file_message(msg[2])
-        USER_SOCKET.sendto(msg, SERVER_ADDR) # base 64
-    print(f'sending {msg} to {SERVER_ADDR}')
+    def handle_message(self, message):
+        if message.startswith('<file'):
+            file_text = self.is_base64(message)
+            file_path = f'output({self.file_counter}).txt'
+            with open(file_path, 'wb') as file:
+                file.write(file_text)
+            self.file_counter += 1
+        print(message)
 
-def login(username):
-    send_message(['login', username])
-
-def logout(username):
-    send_message(['logout', username])
-
-def private_message(username, message):
-    # print('--- private message ----')
-    send_message(['pm', username, message])
-
-def private_message_with_file(username, file):
-    send_message(['pmf', username, file])
+    def handle_tcp_socket(self, socket:socket):
+        message = socket.recv(1024)
+        if not message:  
+            print('closing socket')
+            socket.close()
+            return
+        message = message.decode()
+        self.handle_message(message)
 
 
-def broadcast_message(message):
-    # print('----- broadcasting -----')
-    send_message(['broadcast', message])
+    def handle_udp_socket(self, socket:socket):
+        message, _ = socket.recvfrom(2048)
+        if not message:  
+            print('closing socket')
+            socket.close()
+            return
+        message = message.decode()
+        self.handle_message(message)
 
-def handle_user_input():
-    msg = input()
-    if (msg.startswith('/')):
-        msg = msg[1:].split(' ')
-        if (msg[0] == 'exit'): return -1
-        elif (msg[0] == 'pm'):
-            private_message(msg[1], ' '.join(msg[2:]))
-        elif (msg[0] == 'pmf'):
-            private_message_with_file(msg[1], ' '.join(msg[2:]))
-        elif (msg[0] == 'help'):
-            print('private message           -> /pm [user] <message>')
-            print('private message with file -> /pmf [user] <file>')
-        return 0
-    broadcast_message(msg)
-    return 0
+    def listen(self, socket:socket):
+        socket.settimeout(2)
+        while True:
+            try:
+                if self.SOCKET_TYPE == 'tcp':
+                    self.handle_tcp_socket(socket)
+                else:
+                    self.handle_udp_socket(socket)
+                pass
+            except timeout:
+                if not threading.main_thread().is_alive(): break
+            except Exception as e:
+                print(e)
+                break
 
-def thread_listen():
-    while True:
+    def private_message(self, username, message):
+        self.send_message(['pm', username, self.username, message], self.DATA_SOCKET)
+
+    def private_message_with_file(self, username, file_path):
+        with open(file_path, 'rb') as file:
+            content = file.read()
+        self.send_message(['pmf', username, self.username, base64.b64encode(content)], self.DATA_SOCKET)
+    
+    def login(self, username):
+        self.username = username
+        self.send_message(['login', username], self.CONTROLL_SOCKET)
+
+    def logout(self):
+        self.username = None
+        self.send_message(['logout'], self.CONTROLL_SOCKET)
+    
+
+    def print_help(self):
+        print('login                     -> /login <user>')
+        print('logout                    -> /logout')
+        print('broadcast message         -> /all <message>')
+        print('private message           -> /pm [user] <message>')
+        print('private message with file -> /pmf [user] <file>')
+        
+
+    def broadcast_message(self, message):
+        self.send_message(['broadcast', self.username, message], self.DATA_SOCKET)
+        pass
+
+
+    def handle_user_input(self):
         try:
-            message, _ = USER_SOCKET.recvfrom(2048)
-            message = message.decode()
-            print('<msg> ' + message)
-        except:
-            if not threading.main_thread().is_alive(): break
+            msg = input()
+        except KeyboardInterrupt:
+            return -1
+        join_message = lambda msg: ' '.join(msg)
+        if (msg.startswith('/')):
+            msg = msg[1:].split(' ')
+            option = msg[0]
+            if (option == 'exit'): return -1
+            elif (option == 'pm'): 
+                self.private_message(msg[1], join_message(msg[2:]))
+            elif (option == 'pmf'): 
+                self.private_message_with_file(msg[1], join_message(msg[2:]))
+            elif (option == 'help'):
+                self.print_help()
+            elif (option == 'all'):
+                self.broadcast_message(join_message(msg[1:]))
+            elif (option == 'login'):
+                self.login(msg[1])
+            elif (option == 'logout'):
+                self.logout()
+            # else: print('invalid command')
+        return 0
+
+    def run(self):
+        if (self.SOCKET_TYPE == 'tcp'):
+            print('running tcp client')
+            self.connect()
+        
+        threading.Thread(target=self.listen, args=(self.CONTROLL_SOCKET,)).start()
+        threading.Thread(target=self.listen, args=(self.DATA_SOCKET,)).start()
+
+        while True:
+            if (self.handle_user_input() == -1): break
 
 def main():
-    username = input('Enter your username: ')
-    login(username)
-    x = threading.Thread(target=thread_listen)
-    x.start()
-    while True:
-        if(handle_user_input() == -1): break
-    logout(username)
+    server_ip = argv[1]
+    protocol  = argv[2]
+    client = Client(server_ip, 12000, protocol)
+    try:
+        client.run()
+    except timeout:
+        print('teste')
+    except KeyboardInterrupt:
+        pass
+    print('closing client')
+    client.close()
+
 
 main()
